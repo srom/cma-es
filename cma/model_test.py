270beac43e5629a87ad61d0235c572d9e7d8895d
srom <romain.strock@gmail.com>
2019-10-06 12:10:04 +0100

    Add test using the Branin Function

--- cma/model_test.py
+++ cma/model_test.py
@@ -43,6 +43,50 @@ class TestCMA(unittest.TestCase):
         )
         self.assertTrue(cond)
 
+    def test_branin_fn(self):
+        tf.random.set_seed(444)
+
+        def fitness_fn(x):
+            """
+            Branin Function
+            https://www.sfu.ca/~ssurjano/branin.html
+            """
+            a = 1.
+            b = 5.1 / (4 * np.pi**2)
+            c = 5. / np.pi
+            r = 6.
+            s = 10.
+            t = 1 / (8 * np.pi)
+            return (
+                (a * (x[:,1] - b * x[:,0]**2 + c * x[:,0] - r))**2 +
+                s * (1 - t) * tf.cos(x[:,0]) + s
+            )
+
+        cma = CMA(
+            initial_solution=[3., 7.],
+            initial_step_size=1.,
+            fitness_function=fitness_fn
+        )
+        cma.search(100)
+
+        x1, x2 = cma.best_solution()
+
+        cond = (
+            (
+                np.isclose(x1, -np.pi, rtol=1e-3) and
+                np.isclose(x2, 12.275, rtol=1e-3)
+            ) or
+            (
+                np.isclose(x1, np.pi, rtol=1e-3) and
+                np.isclose(x2, 2.275, rtol=1e-3)
+            ) or
+            (
+                np.isclose(x1, 9.42478, rtol=1e-3) and
+                np.isclose(x2, 2.475, rtol=1e-3)
+            )
+        )
+        self.assertTrue(cond)
+
 
 if __name__ == '__main__':
     unittest.main()
